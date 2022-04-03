a8daa969df2a47c05b0f4cacbe249856aa046e2f
Hakan Gok <hakangok1974@yahoo.com>
2022-04-03 10:40:47 +0100

    initial commit

---
+++ src/work1/HelloWorld.java
@@ -0,0 +1,43 @@
+package work1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(" work1 started " );
+        System.out.println(" created git repository");
+
+    }
+
+/*
+Write a class definition of a class named 'Value' with the following:
+a constructor accepting a single integer parameter
+a constructor with no parameters
+a method 'setVal' that accepts a single parameter,
+a boolean method, 'wasModified' that returns true if setVal was ever called for the object.
+a method 'getVal' that returns an integer value as follows: if setVal has ever been called,
+it getVal returns the last value passed to setVal. Otherwise if the "single int parameter"
+constructor was used to create the object,
+getVal returns the value passed to that constructor. Otherwise getVal returns 0.
+ */
+
+  /*  int param=0;
+    boolean calledMethod =false;
+    //public value(){
+        //parameter
+    //}
+    //public Value (int param){
+        //single parameter
+        this.param=param;
+    }
+
+    public void setValue(int param){
+       // 1 parameter method and is void
+        calledMethod = true;
+    }
+
+    public boolean wasModified(){
+        return calledMethod;
+    }
+    */
+
+
+}

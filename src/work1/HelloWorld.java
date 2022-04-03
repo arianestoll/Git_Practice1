842d4a26e8ff1f6f7d377b2c36b2419962eee5ce
Hakan Gok <hakangok1974@yahoo.com>
2022-04-03 10:42:37 +0100

    new commit

--- src/work1/HelloWorld.java
+++ src/work1/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(" work1 started " );
         System.out.println(" created git repository");
+        System.out.println("new commit ");
 
     }
 
